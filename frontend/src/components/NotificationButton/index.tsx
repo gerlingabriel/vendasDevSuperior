25b4e911bcce781a7e59854c1881b952666ffed5
Gabriel Gerlin <gerlin.gabriel@gmail.com>
2022-07-16 15:12:36 -0300

    Janela de alerta

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import icon from "../../assets/img/notification-icon.svg"
 import './styles.css'
 import axios from 'axios';
 import { BASE_URL } from "../../utils/request";
+import { toast } from "react-toastify";
 
 type Props = {
     salesId: number;
@@ -20,7 +21,7 @@ export default NotificationButton
 function handleClick(id: number): void {
     axios.get(BASE_URL + "/sales/" + id + "/sendSms")
         .then(() => {
-            console.log("Enviado com Sucesso!");
+            toast.info("Msn enviado com Sucesso!")
 
         });
 }
