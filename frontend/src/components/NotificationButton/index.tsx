aa7ed529f2ca7d984f5cddbb90acdb6aea639a7e
Gabriel Gerlin <gerlin.gabriel@gmail.com>
2022-07-16 15:07:18 -0300

    Configurando envio de msn

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,26 @@
 import icon from "../../assets/img/notification-icon.svg"
 import './styles.css'
+import axios from 'axios';
+import { BASE_URL } from "../../utils/request";
 
-function NotificationButton() {
+type Props = {
+    salesId: number;
+}
+
+function NotificationButton( {salesId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(salesId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
 }
 
 export default NotificationButton
+
+function handleClick(id: number): void {
+    axios.get(BASE_URL + "/sales/" + id + "/sendSms")
+        .then(() => {
+            console.log("Enviado com Sucesso!");
+
+        });
+}
